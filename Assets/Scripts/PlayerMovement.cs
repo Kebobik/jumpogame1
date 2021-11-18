ea5e9768698c4cd5964a1cc3054f055792933fb6
Kebobik <kubamalina278@gmail.com>
2021-11-18 08:56:58 +0100

    pohyb do stran

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerControls controls;
+    float direction = 0;
+    public Rigidbody2D playerRB;
+    public float speed = 400;
+    
+    private void Awake()
+    {
+            controls = new PlayerControls();
+            controls.Enable();
+            controls.Land.Move.performed += ctx =>
+            {
+                direction = ctx.ReadValue<float>();
+            };
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.velocity = new Vector2(direction * speed * Time.deltaTime, playerRB.velocity.y);
+    }
+}
