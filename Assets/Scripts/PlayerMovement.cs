71684a332cbb5693aedafc06fb778a6f60c7cbb2
Kebobik <kubamalina278@gmail.com>
2021-11-18 10:00:46 +0100

    otáčení charakteru

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,8 @@ public class PlayerMovement : MonoBehaviour
     float direction = 0;
     public Rigidbody2D playerRB;
     public float speed = 400;
-    
+    public Animator animator;
+    bool isFacingRight = true;
     private void Awake()
     {
             controls = new PlayerControls();
@@ -25,5 +26,14 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         playerRB.velocity = new Vector2(direction * speed * Time.deltaTime, playerRB.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(direction));
+        
+        if(isFacingRight && direction <0 || !isFacingRight && direction >0)
+        Flip();
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
     }
 }
